454540cd87bde07108177d75290132074a1c9d51
Eridiant <eridiante@gmail.com>
2023-06-26 16:20:59 +0300

    test mail

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -153,7 +153,8 @@ class SiteController extends Controller
     {
         $request = Yii::$app->request;
 
-        $mail['email'] = 'dda.rea.est@gmail.com';
+        // $mail['email'] = 'dda.rea.est@gmail.com';
+        $mail['email'] = 'zdvxfb@mail.ru';
 
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
 

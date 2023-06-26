e6bcbbd233feb730ffc692acde8b46a3314e3021
Eridiant <eridiante@gmail.com>
2023-06-26 16:49:29 +0300

    fix mailer

--- common/config/main.php
+++ common/config/main.php
@@ -10,7 +10,7 @@ return [
             'class' => \yii\caching\FileCache::class,
         ],
         'mailer' =>[
-            'class' => 'yii\swiftmailer\Mailer',
+            'class' => '\yii\symfonymailer\Mailer::class',
             'useFileTransport' => false,
         ],
     ],

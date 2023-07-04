5ae12cc23e159e2d6a3513e5520ecb2369d92311
Eridiant <eridiante@gmail.com>
2023-07-04 17:29:10 +0300

    add google tag through the ass

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -42,10 +42,22 @@ AppAsset::register($this);
 	
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KM87BR6');</script>
+    <!-- End Google Tag Manager -->
+
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KM87BR6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <?php require_once('template-header.php'); ?>
 

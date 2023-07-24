95c50a8301649271e3c21bfb172cdf1e5c8ff7a2
Eridiant <eridiante@gmail.com>
2023-07-24 12:50:52 +0300

    links open in new page

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -205,12 +205,12 @@ use yii\helpers\Url;
 <div class="cont-container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
     <div class="cont-wrapper cont-wa">
         <div class="cont-inner">
-            <a href="https://telegram.me/+995555932307" class="cont-wa-link">
+            <a href="https://telegram.me/+995555932307" class="cont-wa-link" target="_blank">
                 <svg width="20" height="20"><use xlink:href="/images/icons.svg#telegram"></use></svg>
             </a>
         </div>
         <div class="cont-inner">
-            <a href="https://wa.me/995555932307" class="cont-wa-link">
+            <a href="https://wa.me/995555932307" class="cont-wa-link" target="_blank">
                 <svg width="20" height="20"><use xlink:href="/images/icons.svg#viber"></use></svg>
             </a>
         </div>

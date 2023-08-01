60563a995214e26d60d2e5d37e41d6305199f274
Eridiant <eridiante@gmail.com>
2023-08-01 13:03:20 +0300

    add chart locale

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,6 +18,7 @@ class AppAsset extends AssetBundle
     ];
     public $js = [
         'js/swiper-bundle.min.js',
+        'js/chart.min.js',
         'js/site.js',
         'js/app.min.js',
     ];
